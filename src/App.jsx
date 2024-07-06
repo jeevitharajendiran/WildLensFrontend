b15cbf34652a7f14ed4e3f87964b47dcef1f2bbe
aswinsenthilkumar20@gmail.com <aswinsenthilkumar20@gmail.com>
2024-07-06 20:18:49 +0530

    Initial Project SetUp - WildLens Tours Frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return ( 
+    <div className="border m-4 p-4 text-center">
+      <p className="text-5xl font-light">Hello World</p>
+    </div>
+   );
+}
+ 
+export default App;
