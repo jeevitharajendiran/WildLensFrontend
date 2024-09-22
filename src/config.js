2211bd2da81ae18d166b1244490961e9d11eab96
jeevitharajendiran <jeevithagct23@gmail.com>
2024-09-22 12:38:46 +0530

    Config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    API_URI: "https://wildlenstours-8au5.onrender.com/api"
+    // API_URI: "https://wildlenstours-8au5.onrender.com/api"
+    API_URI: "http://localhost:3000/api"
 };
 
 export default config;
